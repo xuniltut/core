6d0a7e3307a935df004524a8c35b5803acb92598
rx1310 <rx1310@inbox.ru>
2022-07-25 11:27:19 +0300

    добавляет кнопку ScrollUp

--- @scripts/scroll-event.js
+++ @scripts/scroll-event.js
@@ -1,6 +1,8 @@
 const topbar               = document.querySelector('.topbar-wrapper');
 const topbarScrolled  = "topbar-wrapper--scrolled";
 const topbarTitle = document.querySelector('.topbar__title');
+const topBarScrollUp = document.getElementById('scrollUp');
+const topBarScrollUpDisabled = "topbar__action--disabled";
 
 window.addEventListener('scroll', (e) => {
 
@@ -8,9 +10,12 @@ window.addEventListener('scroll', (e) => {
 		if (window.pageYOffset > 150) {
 			topbar.classList.add(topbarScrolled);
 			topbarTitle.classList.remove('hidden');
+			topBarScrollUp.classList.remove(topBarScrollUpDisabled);
 		} else {
 			topbar.classList.remove(topbarScrolled);
 			topbarTitle.classList.add('hidden');
+			topBarScrollUp.classList.add(topBarScrollUpDisabled);
+
 		}
 	} catch (error) {
 		console.log("TutLinux: " + error);
